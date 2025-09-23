49f02dca762bca955449d0aa8e46c6cf0c49899a
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2025-09-23 11:04:26 -0300

    Create UnitTests.scala

---
+++ src/test/scala/unit/UnitTests.scala
@@ -0,0 +1,34 @@
+package unit
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+/**
+ * Unit tests for Scala Spark Big Data project
+ * TODO: Implementar testes unitários para componentes do sistema
+ * TODO: Adicionar testes para transformações de dados
+ * TODO: Configurar mocks para testes Spark
+ */
+object UnitTests extends AnyFunSuite with Matchers {
+
+  // TODO: Implementar suite de testes
+  test("sample test - should always pass") {
+    val result = 1 + 1
+    result shouldBe 2
+  }
+
+  // TODO: Adicionar testes para processamento de dados
+  test("data processing test - placeholder") {
+    pending // TODO: Implementar teste real
+  }
+
+  // TODO: Adicionar testes para validação de schema
+  test("schema validation test - placeholder") {
+    pending // TODO: Implementar teste real  
+  }
+
+  // TODO: Adicionar testes para transformações Spark
+  test("spark transformations test - placeholder") {
+    pending // TODO: Implementar teste real
+  }
+}
