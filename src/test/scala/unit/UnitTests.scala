0cfe079668709a3dfec120b0a1bc62ea47c321ba
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2025-09-23 13:19:56 -0300

    Add real Spark transformation test for price normalizationAdd real Spark transformation test for price normalizationUpdate UnitTests.scala
    Implemented a functional test that replaces a placeholder TODO with a real Spark transformation test. The test demonstrates min-max normalization of price data using Spark DataFrame operations, including aggregation functions and column transformations. This provides a practical example of testing Spark data transformations.

--- src/test/scala/unit/UnitTests.scala
+++ src/test/scala/unit/UnitTests.scala
@@ -1,8 +1,6 @@
 package unit
-
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
-
 /**
  * Unit tests for Scala Spark Big Data project
  * TODO: Implementar testes unitários para componentes do sistema
@@ -10,7 +8,6 @@ import org.scalatest.matchers.should.Matchers
  * TODO: Configurar mocks para testes Spark
  */
 object UnitTests extends AnyFunSuite with Matchers {
-
   // TODO: Implementar suite de testes
   test("sample test - should always pass") {
     val result = 1 + 1
@@ -27,8 +24,26 @@ object UnitTests extends AnyFunSuite with Matchers {
     pending // TODO: Implementar teste real  
   }
 
-  // TODO: Adicionar testes para transformações Spark
-  test("spark transformations test - placeholder") {
-    pending // TODO: Implementar teste real
+  // Implementação real de teste de transformação Spark
+  test("should normalize price column by min-max") {
+    import org.apache.spark.sql.SparkSession
+    import org.apache.spark.sql.functions._
+
+    val spark = SparkSession.builder().master("local").getOrCreate()
+    import spark.implicits._
+    val df = Seq((1, 10.0), (2, 20.0), (3, 30.0)).toDF("id", "price")
+
+    val minVal = df.agg(min($"price")).as[Double].first
+    val maxVal = df.agg(max($"price")).as[Double].first
+
+    val dfNormalized = df.withColumn("normalized_price",
+      (col("price") - minVal) / (maxVal - minVal)
+    )
+
+    val result = dfNormalized.select("normalized_price").as[Double].collect
+    result(0) shouldEqual 0.0
+    result(1) shouldEqual 0.5
+    result(2) shouldEqual 1.0
+    spark.stop()
   }
 }
